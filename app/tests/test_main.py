2e6028874a8e324e7204c527cda93d97fd03a48e
Omid <omidk14@gmail.com>
2022-01-09 02:41:23 +0330

    array to flat array

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,6 +9,15 @@ def test_main_resource():
     assert response_auth.status_code == 200
 
 
-def test_child_resource():
-    response_auth = client.get(f"/api/v1/flatten/af-v1")
-    assert response_auth.status_code == 200
+def test_af_v1():
+    response= client.post(f"/api/v1/flatten/af-v1",
+                                json=[1.3,[2,[5,[6]]],[7],8])
+    assert response.status_code == 200
+    assert response.json() == [1.3,2,5,6,7,8]
+
+def test_af_v2():
+    response= client.post(f"/api/v1/flatten/af-v2",
+                          json=[1.3,[2,[5,[6]]],[7],8])
+    assert response.status_code == 200
+    assert response.json() == [1.3,2,5,6,7,8]
+
