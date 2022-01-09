72c0a6f9071846a2bb7a9e809fd9575987b4f8ea
Omid <omidk14@gmail.com>
2022-01-09 22:32:02 +0330

    add ci

--- app/main.py
+++ app/main.py
@@ -6,13 +6,6 @@ from api.v1.api import router as api_router
 app = FastAPI(title='Serverless Lambda FastAPI',version=0.1,root_path="/dev/")
 
 app.include_router(api_router, prefix="/api/v1")
-
-
-@app.get("/",  tags=["Endpoint Test"])
-def main_endpoint_test():
-    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
-
-
 # to make it work with Amazon Lambda, we create a handler object
 handler = Mangum(app=app)
 
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,12 +3,6 @@ from main import app
 
 client = TestClient(app)
 
-
-def test_main_resource():
-    response_auth = client.get(f"/")
-    assert response_auth.status_code == 200
-
-
 def test_af_v1():
     response= client.post(f"/api/v1/flatten/af-v1",
                                 json=[1.3,[2,[5,[6]]],[7],8])
