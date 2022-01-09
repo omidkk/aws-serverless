8efab2167782afc2356043e7f1cdd56f31a77d40
Omid <omidk14@gmail.com>
2022-01-09 22:58:02 +0330

    add ci

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,14 +4,12 @@ from main import app
 client = TestClient(app)
 
 def test_af_v1():
-    response= client.post(f"/api/v1/flatten/af-v1",
-                                json=[1.3,[2,[5,[6]]],[7],8])
+    response= client.post(f"/api/v1/flatten/af-v1", json={"input":[1.3,[2,[5,[6]]],[7],8]})
     assert response.status_code == 200
-    assert response.json() == [1.3,2,5,6,7,8]
+    assert response.json() == {"output": [1.3,2,5,6,7,8]}
 
 def test_af_v2():
-    response= client.post(f"/api/v1/flatten/af-v2",
-                          json=[1.3,[2,[5,[6]]],[7],8])
+    response= client.post(f"/api/v1/flatten/af-v2", json={"input":[1.3,[2,[5,[6]]],[7],8]})
     assert response.status_code == 200
-    assert response.json() == [1.3,2,5,6,7,8]
+    assert response.json() == {"output": [1.3,2,5,6,7,8]}
 
